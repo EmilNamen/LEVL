7e8717056307a211ccc0c0e02aee4a5c8964cd81
Emil Namen <ec.namen1091@uniandes.edu.co>
2016-02-25 20:11:09 +0100

    quito alerts pmix

--- main.js
+++ main.js
@@ -1416,11 +1416,11 @@ app.controller('MyCtrl', ['$scope', 'Upload', '$timeout','Restangular','$http', 
                 url: 'http://masa.stratigeek.com:3000/api/transactions',
                 data: $scope.transaction
             }).then(function successCallback(response) {
-                alert(JSON.stringify(response));
+                //alert(JSON.stringify(response));
                 // this callback will be called asynchronously
                 // when the response is available
             }, function errorCallback(response) {
-                alert(JSON.stringify(response));
+                //alert(JSON.stringify(response));
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
             });
